5a5f3906d6add589bf994692538d917b24d9d9fe
Eric DeSimone <eric.desimone@cortland.edu>
2018-05-15 17:00:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,5 +3,5 @@ require 'Open-Uri'
 require_relative "../fixtures/kickstarter.html"
 
 def create_project_hash
-  doc = Nokogiri::HTML(open())
+  doc = Nokogiri::HTML(open("../fixtures/kickstarter.html"))
 end
